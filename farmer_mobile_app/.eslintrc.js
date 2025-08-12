14659bcfcfbb8d651e65df390ad9029ecd3bdb22
Thamidu-Premaratna <e.t.niranjana@gmail.com>
2025-08-12 09:13:16 +0530

    chore: updated configuraitons and added eslint cz prettier

---
+++ farmer_mobile_app/.eslintrc.js
@@ -0,0 +1,62 @@
+module.exports = {
+  root: true,
+  extends: [
+    '@react-native',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'react-native'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  env: {
+    'react-native/react-native': true,
+    es6: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    // TypeScript specific rules
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'warn',
+
+    // React specific rules
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+
+    // React Native specific rules
+    'react-native/no-unused-styles': 'error',
+    'react-native/split-platform-components': 'error',
+    'react-native/no-inline-styles': 'warn',
+    'react-native/no-color-literals': 'warn',
+    'react-native/no-raw-text': 'off',
+    'react-native/sort-styles': 'off',
+
+    // General rules
+    'no-console': 'warn',
+    'no-debugger': 'error',
+    'prefer-const': 'error',
+    'no-var': 'error',
+  },
+  ignorePatterns: [
+    'node_modules/',
+    '.expo/',
+    'android/',
+    'ios/',
+    'dist/',
+    'build/',
+    '*.config.js',
+    '*.config.ts',
+  ],
+};
