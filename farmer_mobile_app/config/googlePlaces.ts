43d8606beb219b9e91553cb66300ab9844787e4c
vishath Amarasinghe <vishathakila2001@gmail.com>
2025-08-14 20:52:37 +0530

    updated configs

--- farmer_mobile_app/config/googlePlaces.ts
+++ farmer_mobile_app/config/googlePlaces.ts
@@ -1,6 +1,6 @@
 // Google Places API Configuration
 export const GOOGLE_PLACES_CONFIG = {
-  apiKey: 'AIzaSyCTGvNTEFOCKhLuYiBhnbUm8dWh9BZ13Sw',
+  apiKey: '',
   language: 'en',
   types: 'geocode',
   minLength: 2,
